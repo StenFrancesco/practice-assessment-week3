0998391a8a3d2a5fa0bc620160398540639025bd
ninxi <stendert_baars@hotmail.com>
2020-03-02 16:16:34 +0100

    add patient signup form

--- src/PatientSignUp.js
+++ src/PatientSignUp.js
@@ -1,10 +1,47 @@
 import React from 'react';
 import './App.css';
+import { useForm } from 'react-hook-form';
 
 function PatientSignUp() {
+
+  const { register, handleSubmit } = useForm();
+  const onSubmit = submitted_data => {
+    console.log(submitted_data)
+  }
+
   return (
     <div>
       <h1>Patient Signup</h1>
+      <form onSubmit={handleSubmit(onSubmit)}>
+        <p>
+          <label>first name: </label>
+          <input ref={register} type="text" name="firstName"></input>
+        </p>
+        <p>
+          <label>last name: </label>
+          <input ref={register} type="text" name="lastName"></input>
+        </p>
+        <p>
+          <label>e-mail: </label>
+          <input ref={register} type="text" name="e-mail"></input>
+        </p>
+        <p>
+          <label>phone: </label>
+          <input ref={register} type="text" name="phone"></input>
+        </p>
+        <p>
+          <label>gender: </label>
+          <select id="gender" ref={register} name="gender">
+            <option value="male">Male</option>
+            <option value="female">Female</option>
+          </select>
+        </p>
+        <p>
+          <label>date of birth: </label>
+          <input ref={register} type="date" name="birthdate"></input>
+        </p>
+        <button>Submit</button>
+      </form>
     </div>
   );
 }
--- src/components/Appointment.js
+++ src/components/Appointment.js
@@ -2,9 +2,27 @@ import React from 'react'
 import '../App.css'
 
 function Appointment() {
+
+  const checkOpenStatus = function () {
+
+    let date = new Date();
+    let hours = date.getHours();
+    let day = date.getDay();
+    let min = date.getMinutes();
+
+    const openclosed = ((hours > 8) && (hours < 17) && (day != 0)) ? "We are open" : "We are closed";
+
+    console.log(openclosed)
+
+    setTimeout(checkOpenStatus, 15000);
+
+  };
+
+  checkOpenStatus();
+
   return (
     <div>
-      <p>We are: (open or closed)</p>
+      <p>{}</p>
       <p>To make an appointment</p>
       <p>call: 020 555 5555</p>
     </div>
