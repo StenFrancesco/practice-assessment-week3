39036b37bed90ee6548841a7c4e1b95c7a4ca0ba
ninxi <stendert_baars@hotmail.com>
2020-03-02 12:27:39 +0100

    add routing

--- src/App.js
+++ src/App.js
@@ -1,17 +1,26 @@
 import React from 'react';
+import { BrowserRouter as Router, Switch, Route } from "react-router-dom";
 import './App.css';
 import PatientSignUp from './PatientSignUp'
 import DutyPage from './DutyPage'
 import PatientDatabase from './PatientDatabase'
 import HomePage from './HomePage'
-import NavBar from '../components/NavBar'
+import NavBar from './components/NavBar'
 
 
 function App() {
   return (
-    <div className="App">
-
-    </div>
+    <Router>
+      <div className="App">
+        <NavBar />
+        <Switch>
+          <Route path="/" exact={true} component={HomePage} />
+          <Route path="/dutypage" exact={true} component={DutyPage} />
+          <Route path="/patientsignup" exact={true} component={PatientSignUp} />
+          <Route path="/patientdatabase" exact={true} component={PatientDatabase} />
+        </Switch>
+      </div>
+    </Router>
   );
 }
 
---
+++ src/components/NavBar.js
@@ -0,0 +1,38 @@
+import React from 'react';
+import '../App.css';
+import { Link, NavLink } from "react-router-dom";
+
+function NavBar() {
+  return (
+    <nav>
+      <ul>
+        <NavLink to='/' activeClassName="selected" activeStyle={{
+          fontWeight: "bold",
+          color: "white"
+        }}>
+          <li>Home</li>
+        </NavLink>
+        <NavLink to='/dutypage' activeClassName="selected" activeStyle={{
+          fontWeight: "bold",
+          color: "white"
+        }}>
+          <li>Doctor Schedule</li>
+        </NavLink>
+        <NavLink to='/patientsignup' activeClassName="selected" activeStyle={{
+          fontWeight: "bold",
+          color: "white"
+        }}>
+          <li>Patient Signup</li>
+        </NavLink>
+        <NavLink to='/patientdatabase' activeClassName="selected" activeStyle={{
+          fontWeight: "bold",
+          color: "white"
+        }}>
+          <li>Patient Database</li>
+        </NavLink>
+      </ul>
+    </nav>
+  );
+}
+
+export default NavBar;
